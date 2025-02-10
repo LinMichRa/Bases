86dab511c5935724975976360a06a56c3756cef1
LinMichRa <lramirezd2@ucentral.edu.co>
2025-02-10 18:52:02 -0500

    class hero complemented

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent {
 
+export class HeroComponent {
+  public name : string ="Ironman"
+  public age: number=45;
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
 }
